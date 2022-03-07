a3666afa45cf1f0a787194fb855bc5fa9cee88ff
The-H3ll <molabhai@student.1337.ma>
2022-03-07 14:29:24 +0100

    Containers

--- main.cpp
+++ main.cpp
@@ -7,9 +7,23 @@
 int     main()
 {
     std::vector<int> first;
-    std::vector<int> second(first);
+    std::vector<char> firsts;
+    std::vector<double> firstss;
+    std::vector<long> firstsss;
 
+    first.shrink_to_fit();
+    
     ft::vector<int> test;
-
-    std::cout << test.size() << std::endl;
+    ft::vector<char> tests;
+    ft::vector<double> testss;
+    ft::vector<long> testsss;
+  //  std::cout << test.size() << std::endl
+    // std::cout << first.max_size() << std::endl;
+    // std::cout << firsts.max_size() << std::endl;
+    // std::cout << firstss.max_size() << std::endl;
+    std::cout << first.max_size() << " || " << test.max_size() <<  std::endl;
+    std::cout << firsts.max_size() << " || " << tests.max_size() <<  std::endl;
+    std::cout << firstss.max_size() << " || " << testss.max_size() <<  std::endl;
+    std::cout << firstsss.max_size() << " || " << testsss.max_size() <<  std::endl;
+    return 0;
 }
--- vector.hpp
+++ vector.hpp
@@ -2,33 +2,39 @@
 #include <memory>
 #include <algorithm>
 #include <iostream>
+#include <cstring>
 
 namespace ft
 {
     template < class T, class Alloc = std::allocator<T> > 
     class vector
     {
+        typedef  std::size_t        size_type;
+        typedef const std::allocator<T>           allocator_type;        
+        typedef typename std::allocator<T>::value_type  value_type;
+
         private:
-        int size;
-        T   *arr;
+            int siz;
+            T   *arr;
         public:
             explicit vector(const allocator_type& alloc = allocator_type())
             {
-                this->size = 0;
-                this->arr = alloc.allocate(0);
+                siz = 0;
+             //   arr = alloc.allocate(0);
             }
             explicit vector(size_type n, const value_type& va = value_type(), const allocator_type& alloc = allocator_type())
             {
                 this->arr = alloc.allocate(n);
                 for (int i = 0; i < n; i++)
                 {
-                    this->arr[i] = val; 
+                    this->arr[i] = va; 
                 }
             }
             template <class InputIterator>
             vector (InputIterator first, InputIterator last, const allocator_type& alloc = allocator_type())
             {
-                int count;
+                int count = 0;
+                int i = 0;
                 while (first != last)
                 {
                     count += 1;
@@ -44,12 +50,39 @@ namespace ft
             vector (const vector& x)
             {
                 this->arr = x.arr;
-                this->size = x.size;
+                this->siz = x.siz;
             }
 
             size_type size() const
             {
-                return (this->size);
+                return (this->siz);
             }
-    };    
+            size_type max_size() const
+            {
+                unsigned long  hold = 1;
+                const char *s = typeid(T).name();
+
+                if (strcmp(s, "i") == 0)
+                {
+                    for (int i = 0; i <62; i++ )
+                        hold *= 2;
+                    return (hold - 1);
+                }
+                else if (strcmp(s, "c") == 0)
+                {
+                    for (int i = 0; i < (64 - sizeof(char)); i++ )
+                        hold *= 2;
+                    return (hold - 1);
+                }
+                else if (strcmp(s, "d") == 0 || strcmp(s, "l") == 0)
+                {
+                     for (int i = 0; i < 61 ; i++ )
+                     {
+                         hold *= 2;
+                     }
+                     return (hold - 1);
+                }   
+                return 0;
+            }
+    };
 }
